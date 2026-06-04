4242fe33de78e03270c550ef66727e9f8c40158a
Pedro0604 <pedrospadari0604@gmail.com>
2026-06-04 19:05:12 -0300

    Solución para la práctica 2

--- IoT/Practicas/Practicas-resueltas/Practica-02-estacion-de-monitoreo-ambiental/practica-02/config.hpp
+++ IoT/Practicas/Practicas-resueltas/Practica-02-estacion-de-monitoreo-ambiental/practica-02/config.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
 const int dhtPin = 26;
-const int ledPin = 2;
-char const* const wifiManagerAPSSID = "ESP32-AP-TP-PedroS";
+const int ldrPin = 33;
+
+const int readInterval = 1000;
+
+char const *const wifiManagerAPSSID = "ESP32-AP-TP-PedroS";
+
+char const *const mqttServer = "192.168.0.217";
+const int mqttPort = 1883;
--- IoT/Practicas/Practicas-resueltas/Practica-02-estacion-de-monitoreo-ambiental/practica-02/practica-02.ino
+++ IoT/Practicas/Practicas-resueltas/Practica-02-estacion-de-monitoreo-ambiental/practica-02/practica-02.ino
@@ -1,23 +1,65 @@
+#include <DHT.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
 #include "config.hpp"
 #include "dht.hpp"
-#include "fs.hpp"
+#include "mq135.hpp"
 #include "conectar_wifi.hpp"
-#include "servidor.hpp"
 
-extern const int ledPin;
+extern const int ldrPin;
+extern char const *const mqttServer;
+extern const int mqttPort;
+extern const int readInterval;
+extern DHT dht;
 
-void setup() {
+unsigned long lastReadTime = 0;
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+void setup()
+{
   Serial.begin(115200);
 
   conectarWiFi();
 
-  pinMode(ledPin, OUTPUT);
-
   iniciarDHT();
-  iniciarFS();
+  iniciarMQ135();
+  pinMode(ldrPin, INPUT);
 
-  iniciarServer();
+  mqttClient.setServer(mqttServer, mqttPort);
+  if (mqttClient.connect("ESP32-Estacion-Monitoreo-Ambiental"))
+  {
+    Serial.println("Conectado exitosamente a MQTT");
+  }
+  else
+  {
+    Serial.print("Fallo la conexión a MQTT, estado: ");
+    Serial.println(mqttClient.state());
+  }
 }
 
-void loop() {
+void loop()
+{
+
+  mqttClient.loop();
+  if (millis() - lastReadTime > readInterval)
+  {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    float calidadAire = leerCalidadDeAire();
+    int ldrValue = analogRead(ldrPin);
+
+    char payload[100];
+    sprintf(payload, "{\"temp\": %.2f, \"hum\": %.2f, \"air\": %.2f, \"light\": %d}", temperature, humidity, calidadAire, ldrValue);
+    if (mqttClient.connected())
+    {
+      mqttClient.publish("iot/ambiente", payload);
+    } else {
+      Serial.println("No se pudo enviar a MQTT, el cliente no está conectado.");
+    }
+
+    lastReadTime = millis();
+  }
 }
